a560523f419a69ea98e672d18ced5dc84e18c72b
4rt-wiz4rd <agds_dantas@outlook.com>
2025-01-15 10:15:01 -0300

    corrigindo o app JS

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ console.log("aplicação está  se conectando ao banco de dados")
 
 //mais um console log de teste: 
 console.log("O Paulo fez esse commit sem voce ver na aula!")
-console.log("Enviando para o Github")
+console.log("Enviando para o Githubb") 
